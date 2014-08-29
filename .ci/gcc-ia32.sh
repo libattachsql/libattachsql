32bcb96790142bfb038a706e88faa3f7eb5e0503
Andrew Hutchings <andrew@linuxjedi.co.uk>
2014-08-29 13:41:35 +0100

    Fix Travis for weird apt bug

--- .ci/gcc-ia32.sh
+++ .ci/gcc-ia32.sh
@@ -1,3 +1,5 @@
+sudo apt-get install -y build-essential g++ gcc libtool
+
 sudo apt-get install -y libc6-dev libc6-dev:i386 libuv-dev:i386 zlib1g-dev:i386
 
 sudo apt-get install -y gcc-multilib g++-multilib
