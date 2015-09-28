6694d2ced6eecde4b6dc93273c543efc9c9cb66f
libla <libla@qq.com>
2015-09-28 21:25:18 +0800

    change ci script to libuv 1.0

--- .ci/clang.sh
+++ .ci/clang.sh
@@ -1,7 +1,9 @@
-sudo apt-get install -y libuv-dev zlib1g-dev clang libssl-dev
+sudo apt-get install -y zlib1g-dev clang libssl-dev
 
 CC="clang"
 CXX="clang++"
+.ci/install-libuv.sh
 autoreconf -fi
 ./configure --enable-debug
+make
 make check 2>/dev/null
